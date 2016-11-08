d8cd3e3e6920ce094c71a23e5688a7dd9d8a9ee3
Javier Cacheiro <jlopez@cesga.es>
2016-11-08 21:00:09 +0100

    Fixes and configuration

--- config/development.py
+++ config/development.py
@@ -8,6 +8,5 @@ SECRET_KEYS = '/etc/keyczar/keys'
 # Fill as needed
 DEBUG = True
 IGNORE_AUTH = True
-MESOS_FRAMEWORK_ENDPOINT = 'http://framework:6001/bigdata/mesos_framework/v1/clusters'
-ORCHESTRATOR_ENDPOINT = 'http://orchestrator:6002/v1/clusters'
 CONSUL_ENDPOINT = 'http://consul:8500/v1/kv'
+START_JUPYTER_SCRIPT = 'scripts/start_jupyter_without_password'
--- config/testing.py
+++ config/testing.py
@@ -1,3 +1,4 @@
+
 # Secret passphrase
 # FIXME: Temporarily SECRET must have the same value as SECRET_KEYS
 #        due to the current spring boot implementation
@@ -7,7 +8,5 @@ SECRET_KEYS = '/etc/keyczar/keys'
 # Fill as needed
 DEBUG = True
 IGNORE_AUTH = False
-# SECRET_KEY = 'admin'
-MESOS_FRAMEWORK_ENDPOINT = 'http://framework:6001/bigdata/mesos_framework/v1/clusters'
-ORCHESTRATOR_ENDPOINT = 'http://orchestrator:6002/v1/clusters'
 CONSUL_ENDPOINT = 'http://consul:8500/v1/kv'
+START_JUPYTER_SCRIPT = 'scripts/start_jupyter_without_password'
